70eb8baf81155964733a245fd0b47266837d5831
beresdev <beresdev@gmail.com>
2023-05-15 13:51:18 -0500

    barra de busqueda, filtra los productos conforme se va escribiendo en el input

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { ProductsList } from './components/ProductsList/ProductsList';
 import { ProductCard } from './components/ProductCard/PorductCard';
+import { SearchBar } from './components/SearchBar/SearchBar';
 
 function App() {
   const [products, setProducts] = useState([]);
-
-  let productsCopy = [...products];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     fetch('https://dummyjson.com/products?limit=100')
     .then(res => res.json())
-    .then(data => setProducts(data.products));
+    .then(data => {
+      setProducts(data.products)
+      setFilteredProducts(data.products);
+     });
   },[])
 
-  //console.log(productsCopy)
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    const filtered = products.filter(product =>
+      product.title.toLowerCase().includes(term)
+    );
+    setFilteredProducts(filtered);
+  }
 
   return (
     <>
+    <SearchBar handleSearch={handleSearch} searchTerm={searchTerm} />
     <ProductsList>
       {
-        productsCopy.map(product => (
+        filteredProducts.map(product => (
           <ProductCard 
           key={product.id}
           title={product.title}
